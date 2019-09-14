373710e02afc87e270f7e25f671085c1a026c5aa
svyat935 <svyat935@mail.ru>
2019-09-14 18:27:52 +0400

    Докручиваем

--- BaseProgram/BaseProgram/Form1.Designer.cs
+++ BaseProgram/BaseProgram/Form1.Designer.cs
@@ -28,37 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.addressFile = new System.Windows.Forms.TextBox();
             this.textInput = new System.Windows.Forms.TextBox();
             this.addressFile_button = new System.Windows.Forms.Button();
             this.writeFile_button = new System.Windows.Forms.Button();
             this.informationTable = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // addressFile
             // 
             this.addressFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.addressFile.Location = new System.Drawing.Point(9, 10);
-            this.addressFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addressFile.Location = new System.Drawing.Point(12, 12);
+            this.addressFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addressFile.Name = "addressFile";
-            this.addressFile.Size = new System.Drawing.Size(492, 32);
+            this.addressFile.Size = new System.Drawing.Size(655, 38);
             this.addressFile.TabIndex = 0;
             // 
             // textInput
             // 
             this.textInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textInput.Location = new System.Drawing.Point(9, 46);
-            this.textInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textInput.Location = new System.Drawing.Point(12, 57);
+            this.textInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textInput.Name = "textInput";
-            this.textInput.Size = new System.Drawing.Size(492, 32);
+            this.textInput.Size = new System.Drawing.Size(655, 38);
             this.textInput.TabIndex = 1;
             // 
             // addressFile_button
             // 
-            this.addressFile_button.Location = new System.Drawing.Point(504, 10);
-            this.addressFile_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addressFile_button.Location = new System.Drawing.Point(672, 12);
+            this.addressFile_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addressFile_button.Name = "addressFile_button";
-            this.addressFile_button.Size = new System.Drawing.Size(87, 31);
+            this.addressFile_button.Size = new System.Drawing.Size(116, 38);
             this.addressFile_button.TabIndex = 2;
             this.addressFile_button.Text = "Путь к файлу";
             this.addressFile_button.UseVisualStyleBackColor = true;
@@ -66,10 +68,10 @@
             // 
             // writeFile_button
             // 
-            this.writeFile_button.Location = new System.Drawing.Point(504, 46);
-            this.writeFile_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.writeFile_button.Location = new System.Drawing.Point(672, 57);
+            this.writeFile_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.writeFile_button.Name = "writeFile_button";
-            this.writeFile_button.Size = new System.Drawing.Size(87, 31);
+            this.writeFile_button.Size = new System.Drawing.Size(116, 38);
             this.writeFile_button.TabIndex = 3;
             this.writeFile_button.Text = "Записать";
             this.writeFile_button.UseVisualStyleBackColor = true;
@@ -78,24 +80,28 @@
             // informationTable
             // 
             this.informationTable.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.informationTable.Location = new System.Drawing.Point(9, 82);
-            this.informationTable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.informationTable.Location = new System.Drawing.Point(12, 101);
             this.informationTable.Name = "informationTable";
-            this.informationTable.Size = new System.Drawing.Size(582, 276);
+            this.informationTable.Size = new System.Drawing.Size(776, 340);
             this.informationTable.TabIndex = 4;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.informationTable);
             this.Controls.Add(this.writeFile_button);
             this.Controls.Add(this.addressFile_button);
             this.Controls.Add(this.textInput);
             this.Controls.Add(this.addressFile);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Base Program";
             this.ResumeLayout(false);
@@ -110,6 +116,7 @@
         private System.Windows.Forms.Button addressFile_button;
         private System.Windows.Forms.Button writeFile_button;
         private System.Windows.Forms.Label informationTable;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- BaseProgram/BaseProgram/Form1.cs
+++ BaseProgram/BaseProgram/Form1.cs
@@ -12,25 +12,39 @@ namespace BaseProgram
 {
     public partial class Form1 : Form
     {
+        private bool flag = false;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private bool flag = false;
+        }        
 
         private void writeFile_button_Click(object sender, EventArgs e)
         {
-            flag = true;
-            string str1 = DateTime.Now.ToString() + "    ||    " + SystemInformation.ComputerName + "    ||    " + textInput.Text + "\n";
             CText cl = new CText();
+            string str1 = DateTime.Now.ToString() + " || " + SystemInformation.ComputerName + " || " + textInput.Text + "\n";
+            
             cl.WFile(addressFile.Text, str1);
+            
             MessageBox.Show("Успешно записано!", "Сообщение", MessageBoxButtons.OK);
+
+            flag = true;
         }
 
         private void AddressFile_button_Click(object sender, EventArgs e)
         {
+            OpenFileDialog pfg = new OpenFileDialog();
+            pfg.ShowDialog();
+            addressFile.Text = pfg.FileName;
+        }
 
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                informationTable.Text += DateTime.Now.ToString() + " || " + SystemInformation.ComputerName + " || " + textInput.Text + "\n";
+                flag = false;
+            }            
         }
     }
 }
