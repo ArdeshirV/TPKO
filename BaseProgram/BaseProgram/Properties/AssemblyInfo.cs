78ad73256a0483293e252f338cacba01f0dad225
ArdeshirV <ArdeshirV@protonmail.com>
2019-11-30 21:37:43 +0330

    Added author name to the copyright line

--- BaseProgram/BaseProgram/Properties/AssemblyInfo.cs
+++ BaseProgram/BaseProgram/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BaseProgram")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Kotyakhov Svyatoslav & ArdeshirV")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
